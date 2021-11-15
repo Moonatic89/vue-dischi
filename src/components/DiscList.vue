0a55bfbc78e015387769842cc5816d34821b4004
Moonatic89 <84795027+Moonatic89@users.noreply.github.com>
2021-11-15 12:49:15 +0100

    Create DiscList Prototype

--- src/components/DiscList.vue
+++ src/components/DiscList.vue
@@ -1,8 +1,26 @@
-<template><div></div></template>
+<template>
+  <div id="DiscList">
+    <img :src="discImg" alt="" />
+    <h2>{{ discName }}</h2>
+    <span>{{ discAuthor }}</span>
+    <span>{{ discYear }}</span>
+  </div>
+</template>
 
 <script>
-export default {};
+export default {
+  props: {
+    discImg: String,
+    discName: String,
+    discAuthor: String,
+    discYear: String,
+  },
+};
 </script>
 
 <style>
+#DiscList {
+  display: flex;
+  flex-direction: column;
+}
 </style>
