668b9ed008b4fcdc373e70513ba362d104114abe
Moonatic89 <84795027+Moonatic89@users.noreply.github.com>
2021-11-16 15:55:21 +0100

    TEST Make two siblings component communicate

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <SiteHeader />
+    <SiteHeader @genreFilter="test" />
     <SiteMain />
   </div>
 </template>
@@ -14,6 +14,11 @@ export default {
   components: {
     SiteHeader,
     SiteMain,
+  },
+  methods: {
+    test() {
+      console.log();
+    },
   },
 };
 </script>
