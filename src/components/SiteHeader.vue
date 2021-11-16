98a060c6f963599d0d28ad52c50aad9f7b21bcc2
Moonatic89 <84795027+Moonatic89@users.noreply.github.com>
2021-11-16 15:24:44 +0100

    Generate Array of unique genres for dynamic option

--- src/components/SiteHeader.vue
+++ src/components/SiteHeader.vue
@@ -18,7 +18,39 @@
 
 
 <script>
-export default {};
+import axios from "axios";
+export default {
+  data() {
+    return {
+      genresArr: [],
+      authorsArr: [],
+    };
+  },
+  methods: {
+    getGenres(objs) {
+      const uniqueGenres = [];
+      for (let i = 0; i < objs.length; i++) {
+        if (!uniqueGenres.includes(objs[i].genre)) {
+          uniqueGenres.push(objs[i].genre);
+        }
+      }
+      return uniqueGenres;
+    },
+    getAuthors() {},
+  },
+  mounted() {
+    axios
+      .get("https://flynn.boolean.careers/exercises/api/array/music")
+      .then((r) => {
+        this.genresArr = this.getGenres(r.data.response);
+        console.log(this.genresArr);
+
+        // this.genres = r.data.response.genres;
+        // this.authors = r.data.response.authors;
+        // console.log(this.genres, this.authors);
+      });
+  },
+};
 </script>
 
 <style lang="scss">
