e47c58e4760f6963837127e5b6d6d668f77fa80e
Moonatic89 <84795027+Moonatic89@users.noreply.github.com>
2021-11-16 15:28:08 +0100

    TEST Dynamic options working

--- src/components/SiteHeader.vue
+++ src/components/SiteHeader.vue
@@ -7,10 +7,15 @@
       <label for="cars">Choose a car:</label>
 
       <select name="cars" id="cars">
+        <option v-for="genre in genresArr" :key="genre" value="genre">
+          {{ genre }}
+        </option>
+
+        <!--         
         <option value="volvo">Volvo</option>
         <option value="saab">Saab</option>
         <option value="mercedes">Mercedes</option>
-        <option value="audi">Audi</option>
+        <option value="audi">Audi</option> -->
       </select>
     </div>
   </header>
